1de5d90988a72fc24c3053901becd78af15e4695
Rami Isaac <isaacr@spu.edu>
2023-05-11 16:22:47 -0700

    Initialize coveralls

---
+++ src/modules/token/token.interfaces.ts
@@ -0,0 +1,38 @@
+import { Document, Model } from 'mongoose';
+import { JwtPayload } from 'jsonwebtoken';
+
+export interface IToken {
+  token: string;
+  user: string;
+  type: string;
+  expires: Date;
+  blacklisted: boolean;
+}
+
+export interface NewToken {
+  token: string;
+  user: string;
+  type: string;
+  expires: Date;
+}
+
+export interface ITokenDoc extends IToken, Document {}
+
+export interface ITokenModel extends Model<ITokenDoc> {}
+
+export interface IPayload extends JwtPayload {
+  sub: string;
+  iat: number;
+  exp: number;
+  type: string;
+}
+
+export interface TokenPayload {
+  token: string;
+  expires: Date;
+}
+
+export interface AccessAndRefreshTokens {
+  access: TokenPayload;
+  refresh: TokenPayload;
+}
